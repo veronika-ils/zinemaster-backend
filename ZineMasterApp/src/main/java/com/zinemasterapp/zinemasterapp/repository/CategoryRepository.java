5f3a4615b1f933379522afd6946c5e2527b24dd3
Veronika Ilioska <ilioskaveronika@gmail.com>
2025-08-27 21:50:21 +0200

    Delete ZineMasterApp directory

--- ZineMasterApp/src/main/java/com/zinemasterapp/zinemasterapp/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zinemasterapp.zinemasterapp.repository;
-
-import com.zinemasterapp.zinemasterapp.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-}
